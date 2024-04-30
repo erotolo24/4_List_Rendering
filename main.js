a29515af5d3968291bea0cd981bec592ab8bbf8f
erotolo24 <erotolo24@gmail.com>
2024-04-30 11:30:50 -0400

    final commit

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ const app = Vue.createApp({
       image: './assets/images/socks_blue.jpg',
       inStock: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
+      sizes: ['Kids', 'Adult Woman', 'Adult Man'],
       variants: [
         {id: 1111, color: 'green'},
-        {id: 1112, color: 'blue'}
+        {id: 1112, color: 'blue'},
       ]
     }
   }
